ab7f014d885e7ba27be6c67a3a034f335e082f84
mviniciuz <m.viniciuz@hotmail.com>
2024-01-19 17:06:49 -0300

    initial

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,19 @@
+import { View, Image, Text } from "react-native";
+
+import { styles } from './styles';
+
+
+export function Home(){
+  
+  return(
+ 
+    <View style={styles.container} >
+      <View style={styles.header}>
+              
+     
+      </View>    
+
+    </View>
+  
+  )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#1A1A1A', 
+  },  
+
+  header: {
+    height: '30%',
+    width:'100%',
+    backgroundColor: '#0D0D0D',
+  },
+  logo: {
+    width: 'auto',
+    height: 'auto',
+    resizeMode: 'center'
+    
+
+  }
+})
