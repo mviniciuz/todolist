e5533114023429a38bceca38eb1c906d3f6dafd7
marcus <you@example.com>
2024-01-22 22:30:28 -0300

    final

--- src/components/imputTask/index.tsx
+++ src/components/imputTask/index.tsx
@@ -4,7 +4,7 @@ import { styles } from "./styles";
 export function InputTask({value, setValue}){
   return(
     <TextInput style={styles.input}
-      placeholder="Adicione uma nova tarefa"
+      placeholder="Add a new task"
       placeholderTextColor={'#808080'}
       value={value}
       onChangeText={setValue}
